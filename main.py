aae14fe31041fa94daf76f9a892e80b334a687b7
0MAVM0 <131771338+0MAVM0@users.noreply.github.com>
2023-08-14 10:36:15 +0500

    Update 2 main.py
    asd

--- main.py
+++ main.py
@@ -4,54 +4,64 @@ from telegram.ext import *
 
 TOKEN = "6429731092:AAHbdUp9BlQnQ5B0AmWrbUFrSEtBM2N-mvY"
 
-RANDOM_IMAGE = "Random image"
-GET_MP3 = "Get mp3"
-RANDOM_IMG_URL = "https://picsum.photos/1200"
-
-global IMAGE_COUNTER
-IMAGE_COUNTER = 0
-
 print("Running up the bot...")
 
 def start(update, context):
     _send_local_file(update, context)
+    help(update, context)
+
 
 def _send_local_file(update, context):
     with open("me.jpg", "rb") as f:
-        update.message.reply_photo(f, caption="Hello world! This is me!")
+        update.message.reply_photo(f, caption="Hello! This is me!")
 
-def _send_mp3(update: Update, context: CallbackContext):
-    with open("music.mp3", "rb") as f:
-        update.message.reply_audio(f, caption="This is mp3")
-
-def get_buttons(update: Update, context: CallbackContext):
+def get_order_food_message(update: Update, context: CallbackContext):
     buttons = [
-        [KeyboardButton(RANDOM_IMAGE)],
-        [KeyboardButton(GET_MP3)]
+        [KeyboardButton("Best burger of the week")],
+        [KeyboardButton("Cheese burger")],
+        [KeyboardButton("Hamburger")]
     ]
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Choose option:",
+        text="Order food:",
         reply_markup=ReplyKeyboardMarkup(buttons)
     )
 
-def message_handler(update: Update, context: CallbackContext):
-    global IMAGE_COUNTER
-    IMAGE_COUNTER += 1
-    if update.message.text == RANDOM_IMAGE:
-        image = get(RANDOM_IMG_URL).content
-        context.bot.sendMediaGroup(
+def callback_query_handler_for_attached_buttons(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    if query.data == "1":
+        query.edit_message_text(text="You pressed button 1")
+    elif query.data == "2":
+        query.edit_message_text(text="You pressed button 2")
+
+def message_handler_for_below_buttons(update: Update, context: CallbackContext):
+    if update.message.text == "Best burger of the week":
+        context.bot.send_message(
             chat_id=update.effective_chat.id,
-            media=[InputMediaPhoto(image, caption=f"Random {IMAGE_COUNTER}")]
+            text="It costs 99,9$\nYou should pay by electronic card\nCard of 🍔King is: 8801 3062 3759 9944",
         )
-    elif update.message.text == GET_MP3:
-        _send_mp3(update, context)
+    elif update.message.text == "Cheese burger":
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="It costs 49,9$\nYou should pay by electronic card\nCard of 🍔King is: 8801 3062 3759 9944",
+        )
+    elif update.message.text == "Hamburger":
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="It costs 14,9$\nYou should pay by electronic card\nCard of 🍔King is: 8801 3062 3759 9944",
+        )
+    elif update.message.text == "Burger":
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="It costs 9,9$\nYou should pay by electronic card\nCard of 🍔King is: 8801 3062 3759 9944",
+        )
 
 def help(update, context):
     update.message.reply_text("""
-/start   - Start the bot
-/help    - Help
-/buttons - Get Optional buttons
+/start      - Start the bot
+/help       - Help
+/order_food - Order food
 """
                               )
 
@@ -59,8 +69,9 @@ updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("help", help))
-dispatcher.add_handler(CommandHandler('buttons', get_buttons))
-dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+dispatcher.add_handler(CommandHandler('order_food', get_order_food_message))
+dispatcher.add_handler(MessageHandler(Filters.text, message_handler_for_below_buttons))
+dispatcher.add_handler(CallbackQueryHandler(callback_query_handler_for_attached_buttons))
 
 updater.start_polling()
 updater.idle()
